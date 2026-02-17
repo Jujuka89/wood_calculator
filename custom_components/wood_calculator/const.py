6e253d15982fbf6a48c414834f3c9e85e219ad5b
Jujuka89 <74470508+Jujuka89@users.noreply.github.com>
2026-02-17 17:41:00 +0100

    Add constants for wood calculator configuration

--- custom_components/wood_calculator/const.py
+++ custom_components/wood_calculator/const.py
@@ -1,1 +1,7 @@
+DOMAIN = "wood_calculator"
+
+DEFAULT_TEMP_SEUIL = 60
+DEFAULT_DUREE_BUCHE = 30        # minutes = 1 bûche
+DEFAULT_BUCHES_STERE = 250     # nombre de bûches dans 1 stère
+DEFAULT_PRIX_STERE = 90        # € par stère
 
