6982152b81e16daffd0f290ce853614862f8417c
Jujuka89 <74470508+Jujuka89@users.noreply.github.com>
2026-03-01 00:31:17 +0100

    Update const.py

--- custom_components/wood_calculator/const.py
+++ custom_components/wood_calculator/const.py
@@ -5,3 +5,4 @@ DEFAULT_DUREE_BUCHE = 30        # minutes = 1 bûche
 DEFAULT_BUCHES_STERE = 250     # nombre de bûches dans 1 stère
 DEFAULT_PRIX_STERE = 60        # € par stère
 DEFAULT_DEBUT_CHAUFFE_MOIS = 9  # septembre
+
